a4aa555fe8bb61e1d6a3fadc9fa944c31ee9c9c0
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-02-27 09:31:49 -0600

    add bear function works

--- javascripts/components/bearForm.js
+++ javascripts/components/bearForm.js
@@ -1,12 +1,20 @@
 import util from "../helpers/utils.js";
+import bearData from "../helpers/data/bearData.js";
 
-const addNewBear = () => {
-  console.log('bear added:', document.getElementById('input-bear-name').value);
+const addNewBear = (e) => {
+  e.preventDefault();
+  let newBear = {
+    name: document.getElementById('input-bear-name').value,
+    imageURL: document.getElementById('input-bear-image').value
+  };
+  bearData.bears.push(newBear);
+  document.getElementById('new-bear-form').reset();
+  console.log('bears array:', bearData.bears);
 };
 
 const printBearForm = () => {
   let domString = ''
-domString += '<form class="col-6">',
+domString += '<form class="col-6" id="new-bear-form">',
 domString += '  <div class="form-group">',
 domString += '    <label for="inputName">Bear Name</label>',
 domString += '    <input type="text" class="form-control" id="input-bear-name" aria-describedby="textHelp">',
