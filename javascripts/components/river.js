fc1e9b9df27fae5688158a67c6c913d0f1598678
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-02-27 11:14:41 -0600

    buttons in place

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -9,6 +9,11 @@ const printAllBears = () => {
     domString += `      <img src="${bearX.imageUrl}" class="card-img-top" alt="bear photo">`,
     domString += '      <div class="card-body">',
     domString += `        <h5 class="card-title">${bearX.name}</h5>`,
+    domString += '        <p class="card-text">Fishing events:</p>',
+    domString += '        <div class="row">',
+    domString += '        <div class="col-md"><button type="button" class="btn btn-info"><i class="fas fa-times"></i> Attempt</button></div>',
+    domString += '        <div class="col-sm"><button type="button" class="btn btn-success"><i class="fas fa-fish"></i> Catch</button></div>',
+    domString += '        </div>',
     domString += '      </div>',
     domString += '    </div>',
     domString += '  </div>'
