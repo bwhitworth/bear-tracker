f30380196484e56570f7baea468d274be5b125f7
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-02-27 11:52:21 -0600

    buttons work with listeners

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -1,24 +1,44 @@
 import util from "../helpers/utils.js";
 import bearData from "../helpers/data/bearData.js";
 
+const addButtonEvents = () => {
+const attemptButtons = document.getElementsByClassName('attempt-button');
+for(let i = 0; i < attemptButtons.length; i++){
+  attemptButtons[i].addEventListener('click', fishingAttempt);
+};
+const successButtons = document.getElementsByClassName('success-button');
+for(let i = 0; i < successButtons.length; i++){
+  successButtons[i].addEventListener('click', fishingSuccess);
+};
+};
+
+const fishingAttempt = () => {
+  console.log('attempted to catch a fish');
+};
+
+const fishingSuccess = () => {
+  console.log('caught a fish!');
+};
+
 const printAllBears = () => {
   let domString = "";
   bearData.bears.forEach((bearX) =>{
-    domString += '  <div class="col-3">',
-    domString += '    <div class="card">',
+    domString += '  <div class="col-4">',
+    domString += `    <div id="${bearX.id}"class="card">`,
     domString += `      <img src="${bearX.imageUrl}" class="card-img-top" alt="bear photo">`,
     domString += '      <div class="card-body">',
     domString += `        <h5 class="card-title">${bearX.name}</h5>`,
-    domString += '        <p class="card-text">Fishing events:</p>',
+    domString += '        <p class="card-text">Fishing Results:</p>',
     domString += '        <div class="row">',
-    domString += '        <div class="col-md"><button type="button" class="btn btn-info"><i class="fas fa-times"></i> Attempt</button></div>',
-    domString += '        <div class="col-sm"><button type="button" class="btn btn-success"><i class="fas fa-fish"></i> Catch</button></div>',
+    domString += `        <div class="col-md"><button type="button" class="btn btn-secondary attempt-button"><i class="fas fa-times"></i> ${bearX.attempts}</button></div>`,
+    domString += `        <div class="col-sm"><button type="button" class="btn btn-success success-button"><i class="fas fa-fish"></i> ${bearX.successes}</button></div>`,
     domString += '        </div>',
     domString += '      </div>',
     domString += '    </div>',
     domString += '  </div>'
   })
   util.printToDom('river-container', domString);
+  addButtonEvents();
 };
 
 export default { printAllBears };
--- javascripts/helpers/data/bearData.js
+++ javascripts/helpers/data/bearData.js
@@ -1,15 +1,24 @@
 const bears = [
   {
-   name: 'Alpha', 
-   imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.JfM1eEXPpjrAT1c03q6z2gHaHb%26pid%3DApi&f=1"
+    id: 'bear1',
+    name: 'Alpha', 
+    imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.JfM1eEXPpjrAT1c03q6z2gHaHb%26pid%3DApi&f=1",
+    attempts: 1,
+    successes: 0
   },
   {
+    id: 'bear2',
     name: 'Beta', 
-    imageUrl: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.dailymail.co.uk%2Fi%2Fpix%2F2012%2F08%2F27%2Farticle-2194244-06039FDD0000044D-389_634x649.jpg&f=1&nofb=1"
+    imageUrl: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.dailymail.co.uk%2Fi%2Fpix%2F2012%2F08%2F27%2Farticle-2194244-06039FDD0000044D-389_634x649.jpg&f=1&nofb=1",
+    attempts: 0,
+    successes: 1
    },
    {
+    id: 'bear3',
     name: 'Delta', 
-    imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fferrebeekeeper.files.wordpress.com%2F2013%2F10%2Fbrown-bear-female-and-its-children-play-with-a-ball-in-kamchatka-peninsula-russia-801276.jpg&f=1&nofb=1"
+    imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fferrebeekeeper.files.wordpress.com%2F2013%2F10%2Fbrown-bear-female-and-its-children-play-with-a-ball-in-kamchatka-peninsula-russia-801276.jpg&f=1&nofb=1",
+    attempts: 2,
+    successes: 2
    },
 ];
 
